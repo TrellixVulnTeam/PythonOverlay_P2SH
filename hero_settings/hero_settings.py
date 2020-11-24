1fe7fd1fd2ff102011eb629dd745642f92bd05ad
Nicolai Andersen <niiicolai@gmail.com>
2020-11-24 07:09:44 +0100

    remove old hero settings file

--- hero_settings/hero_settings.py
+++
@@ -1,9 +0,0 @@
-"""
-    hero settings defines general configurations for each hero
-"""
-
-# The path to the directory containing small images of the heroes' items
-ITEMS_FILE_PATH = 'assets/images/items/small/'
-
-# The path to the directory containing references images for each heroes' items
-REFERENCES_FILE_PATH = 'assets/images/items/references/'
